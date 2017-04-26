09a216e65396478d17021a5c099879a35f73e397
willjev13 <jevnikarw@gilmour.org>
2017-04-26 11:22:47 -0400

    adjusted scan in is_clear and manuever

--- pigo.py
+++ pigo.py
@@ -159,7 +159,7 @@ class Pigo(object):
 
     def is_clear(self):
         print("Running the is_clear method.")
-        for x in range((self.MIDPOINT - 40), (self.MIDPOINT + 40), 5):
+        for x in range((self.MIDPOINT - 40), (self.MIDPOINT + 40), 10):
             servo(x)
             time.sleep(.1)
             scan1 = us_dist(15)
